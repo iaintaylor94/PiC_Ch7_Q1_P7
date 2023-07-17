3859fd2eb5827d95ae805acefaccda3b9b703cbc
Iain Taylor <iain.taylor.94@gmail.com>
2023-07-17 13:37:35 +0000

    completed program

--- .ccls-cache/@home@runner@Ch7Q1P7/main.c
+++ .ccls-cache/@home@runner@Ch7Q1P7/main.c
@@ -1,6 +1,37 @@
+// Program to convert a positive intiger to another base
+
 #include <stdio.h>
 
 int main(void) {
-  printf("Hello World\n");
+
+  const char baseDigits[16] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+  int convertedNumber[64];
+  long int numberToConvert;
+  int nextDigit, base, index = 0;
+
+  // Get the number and the base
+  printf ("Number to be converted?  ");
+  scanf ("%ld", &numberToConvert);
+  printf ("Base?");
+  scanf ("%d", &base);
+
+  // Convert to indicated basse
+  do {
+    convertedNumber[index] = numberToConvert % base;
+    index++;
+    numberToConvert /= base;
+  } while (numberToConvert != 0);
+
+  // Display results in reverse order
+  printf ("Converted number = ");
+
+  for (--index; index >= 0; --index) {
+    nextDigit = convertedNumber[index];
+    printf ("%c", baseDigits[nextDigit]);
+  }
+
+  printf ("\n");
+
+  
   return 0;
 }
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Programming in C
+## Chapter 7
+### Question 1: Part 7
+
+This problem is from chapter 7 of the textbook, it is to implement program 7.7[^1] from the textbook.
+
+You may wish to run the program and compare its output.
+
+[^1]: Programming in C (Third edition) By Stephen G. Kochan - Page 110 - Program 7.7
